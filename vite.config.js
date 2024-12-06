cc26dbe9f0112d1e626b68b50fd18da0fb0a889f
Yessine Trigui <55018185+yessinetrigui@users.noreply.github.com>
2024-12-06 04:00:06 +0100

    fix code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
