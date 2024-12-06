5542918a1960e4de57e3000955953418d6f03444
Yessine Trigui <55018185+yessinetrigui@users.noreply.github.com>
2024-12-06 04:02:12 +0100

    fix code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
+module.exports = {
+  content: [
   "./index.html", // Include the main HTML file
   "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
